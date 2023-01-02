a066902791ae9e8cd27551ada2d36c8cf99157b5
vadflor <vadflor@gmail.com>
2023-01-02 22:02:14 +0200

    first commit

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+import classes from './main-navigation.module.css';
+import { useSession, signOut } from 'next-auth/react';
+
+function MainNavigation() {
+  // const [session, loading] = useSession();
+  const { data: session, status } = useSession()
+
+  console.log(session);
+  console.log(status);
+
+  function logoutHandler() {
+    signOut();
+  }
+
+  return (
+    <header className={classes.header}>
+      <Link href='/'>
+        <div className={classes.logo}>Next Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!session && <li>
+            <Link href='/auth'>Login</Link>
+          </li>}
+          {session && <li>
+            <Link href='/profile'>Profile</Link>
+          </li>}
+          {session && <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ pages/auth.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import AuthForm from '../components/auth/auth-form';
+
+function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true); 
+  const router = useRouter();
+
+  useEffect(()=>{
+    getSession().then(session => {
+      if(session){
+        router.replace('/');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  },[router]);
+
+  if(isLoading){
+    return <p>Loading...</p>
+  }
+
+  return <AuthForm />;
+}
+
+export default AuthPage;
